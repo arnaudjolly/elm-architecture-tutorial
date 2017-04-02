6b2e38cc0d90ed0040fd84dadc08083285337925
Arnaud Jolly <contact@arnaud-jolly.net>
2017-04-02 17:30:12 +0200

    a solution i'm not happy with

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,6 +1,7 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import Char
 
 
 main =
@@ -19,12 +20,14 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : String
+  , validation: Bool
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" "" False
 
 
 
@@ -35,6 +38,8 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
+    | Submit
 
 
 update : Msg -> Model -> Model
@@ -49,7 +54,11 @@ update msg model =
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Age age ->
+      { model | age = age }
 
+    Submit ->
+      { model | validation = True }
 
 -- VIEW
 
@@ -60,17 +69,99 @@ view model =
     [ input [ type_ "text", placeholder "Name", onInput Name ] []
     , input [ type_ "password", placeholder "Password", onInput Password ] []
     , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , input [ type_ "number", placeholder "Age", onInput Age ] []
+    , button [ onClick Submit ] [ text "submit" ]
     , viewValidation model
     ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+  if not model.validation then
+    div [] [ text "" ]
+  else
+      let
+        (color, messages) =
+            validate model
+      in
+        div [ style [("color", color)] ] [ viewMessages messages ]
+
+
+viewMessages : List String -> Html msg
+viewMessages messages =
+    ul []
+        (List.map
+            (\m -> li [] [ text m ])
+            messages
+        )
+
+activeValidations : List (Model -> Maybe String)
+activeValidations =
+    [ identicalPasswords
+    , checkLength
+    , checkLower
+    , checkUpper
+    , checkAgeIsNumber
+    ]
+
+
+validate : Model -> (String, List String)
+validate model =
+    let
+        errors =
+            List.filterMap
+                (\v -> v model)
+                activeValidations
+    in
+        if List.isEmpty errors then
+            ("green", ["OK"])
+        else
+            ("red", errors)
+
+identicalPasswords : Model -> Maybe String
+identicalPasswords model =
+    if model.password == model.passwordAgain then
+        Nothing
+    else
+        Just "passwords should be identical"
+
+
+checkAgeIsNumber : Model -> Maybe String
+checkAgeIsNumber model =
+    case String.toInt model.age of
+        Err msg ->
+            Just "age should be a number"
+
+        _ ->
+            Nothing
+
+
+
+checkLength : Model -> Maybe String
+checkLength model =
+    if String.length model.password < 8 then
+        Just "passwords should at least be with length 8"
+    else
+        Nothing
+
+checkDigit : Model -> Maybe String
+checkDigit model =
+    checkString Char.isDigit model.password "passwords should contains at least one digit"
+
+
+checkLower : Model -> Maybe String
+checkLower model =
+    checkString Char.isLower model.password "passwords should contains a LowerCase letter"
+
+
+checkUpper : Model -> Maybe String
+checkUpper model =
+    checkString Char.isUpper model.password "passwords should contains an UpperCase letter"
+
+
+checkString : (Char -> Bool) -> String -> String -> Maybe String
+checkString check s error =
+    if String.any check s then
+        Nothing
+    else
+        Just error
